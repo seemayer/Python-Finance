2afd0fe87f30b315889c9267abdfa24a4d9b45a1
seemayer <bradseemayer@gmail.com>
2022-04-25 08:54:57 +0100

    data puller to csv

--- get_data_and_save_to_csv.py
+++ get_data_and_save_to_csv.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import yfinance as yf
 
-symbol = 'TSLA'
-interval = '1d'
+symbol = 'SPY'
+interval = '1mo'
 period = "2y"
 
 # load data and convert date
 df = yf.download(symbol,period=period, interval = interval)
-print(df.tail[6])
+
+df.to_csv(symbol + ".csv")
