9c1bf940111fe25995976ec4581eadb13ceeb805
seemayer <104170471+seemayer@users.noreply.github.com>
2022-04-25 22:41:34 +0000

    fixed chart

--- plot.py
+++ plot.py
@@ -23,27 +23,27 @@ def plot_chart(file):
   fplt.plot(df.signal, ax=ax2, legend='Signal')
   
   # plot price and volume
-  #fplt.candlestick_ochl(df[['Date','Open','Close','High','Low']], ax=ax)
-  #hover_label = fplt.add_legend('', ax=ax)
-  #axo = ax.overlay()
-  #fplt.volume_ocv(df[['Date','Open','Close','Volume']], ax=axo)
-  #fplt.plot(df.Volume.ewm(span=24).mean(), ax=axo, color=1)
+  fplt.candlestick_ochl(df[['Date','Open','Close','High','Low']], ax=ax)
+  hover_label = fplt.add_legend('', ax=ax)
+  axo = ax.overlay()
+  fplt.volume_ocv(df[['Date','Open','Close','Volume']], ax=axo)
+  fplt.plot(df.Volume.ewm(span=24).mean(), ax=axo, color=1)
   
   #######################################################
   ## update crosshair and legend when moving the mouse ##
   
-  #def update_legend_text(x, y):
-      #row = df.loc[df.Date==x]
+  def update_legend_text(x, y):
+      row = df.loc[df.Date==x]
       # format html with the candle and set legend
-      #fmt = '<span style="color:#%s">%%.2f</span>' % ('0b0' if (row.Open<row.Close).all() else 'a00')
-      #rawtxt = '<span style="font-size:13px">%%s %%s</span> &nbsp; O%s C%s H%s L%s' % (fmt, fmt, fmt, fmt)
-      #hover_label.setText(rawtxt % (symbol, interval.upper(), row.Open, row.Close, row.High, row.Low))
+      fmt = '<span style="color:#%s">%%.2f</span>' % ('0b0' if (row.Open<row.Close).all() else 'a00')
+      rawtxt = '<span style="font-size:13px">%%s %%s</span> &nbsp; O%s C%s H%s L%s' % (fmt, fmt, fmt, fmt)
+      hover_label.setText(rawtxt % (symbol, interval.upper(), row.Open, row.Close, row.High, row.Low))
   
-  #def update_crosshair_text(x, y, xtext, ytext):
-      #ytext = '%s (Close%+.2f)' % (ytext, (y - df.iloc[x].Close))
-      #return xtext, ytext
+  def update_crosshair_text(x, y, xtext, ytext):
+      ytext = '%s (Close%+.2f)' % (ytext, (y - df.iloc[x].Close))
+      return xtext, ytext
   
-  #fplt.set_time_inspector(update_legend_text, ax=ax, when='hover')
-  #fplt.add_crosshair_info(update_crosshair_text, ax=ax)
+  fplt.set_time_inspector(update_legend_text, ax=ax, when='hover')
+  fplt.add_crosshair_info(update_crosshair_text, ax=ax)
   
   fplt.show()
