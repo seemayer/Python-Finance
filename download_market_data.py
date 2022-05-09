3a85731046a822f63a544fc9200d4879e15af728
seemayer <bradseemayer@gmail.com>
2022-05-09 22:09:33 +0100

    Added relative strength screen

--- download_market_data.py
+++ download_market_data.py
@@ -4,16 +4,14 @@ import os
 import yfinance as yf
 from yahoo_fin import stock_info as si
 
-# Delete all files in data directory
-for file in os.scandir('./data/'):
-  os.unlink(file.path)
-
-
 def get_data(ticker,interval = '1d',period = '1y'):
-
   # download data from yahoo
   df = yf.download(ticker,period=period, interval = interval)
   return df
+
+# Delete all files in data directory
+for file in os.scandir('./data/'):
+  os.unlink(file.path)
 
 # Download list of market tickers and save to file
 lst_tickers = si.tickers_ftse100() 
@@ -21,7 +19,7 @@ lst_tickers = si.tickers_ftse100()
 lst_tickers = [item + '.L' for item in lst_tickers] #add suffix
 
 for ticker in lst_tickers:
-   
+  
   try:
     print(ticker)
     df = get_data(ticker)
--- elder_triple_screen.py
+++ elder_triple_screen.py
@@ -20,14 +20,11 @@ def reset_screen_files():
     print('Sucessfully copied data to screen passed folder')
   except:
     print('unable to copy over data')
-  
-  
-def df_from_csv(file): 
-  df = pd.read_csv(file.path)
+def df_from_csv(file_path): 
+  df = pd.read_csv(file_path)
   df['Date'] = pd.to_datetime(df['Date'])
   df = df.set_index('Date')
   return df
-
 def resample_weekly(df):
 
     # resample to weekly candles, i.e. five 1-day candles per business week
@@ -36,19 +33,15 @@ def resample_weekly(df):
     dfw['High'] = df.High.resample('W-MON').max()
     dfw['Low'] = df.Low.resample('W-MON').min()
     dfw['Volume'] = df.Volume.resample('W-MON').sum()
-
     dfw = dfw.dropna()
-
     return dfw
-
-
 
 reset_screen_files()
 
 #Screen 1 - Weekly impulse
 for file in os.scandir('./screen passed/'):
   
-  df = df_from_csv(file)
+  df = df_from_csv(file.path)
   dfw = resample_weekly(df)
 
   #calculate indicators for each ticker and add to dataframe
@@ -65,7 +58,7 @@ for file in os.scandir('./screen passed/'):
 #Screen 2 - Daily Force index
 for file in os.scandir('./screen passed/'):
 
-  df = df_from_csv(file)
+  df = df_from_csv(file.path)
 
   #calculate indicators for each ticker and add to dataframe
   df = ai.force_index(df)
