bfd89d3a5bc65e8a84bfa4b22132d3e381bf590e
seemayer <104170471+seemayer@users.noreply.github.com>
2022-06-20 17:51:50 +0000

    fixed date issues

--- main.py
+++ main.py
@@ -7,17 +7,8 @@ import pandas as pd
 
 # CORE PROGRAM START
 
-
-dict_from_csv = pd.read_csv('./naked trades/MAP.csv', header=0, index_col=0).squeeze('columns').to_dict()
+md.download_naked_trades(earliest_date = '2017-01-01')
 df = md.df_from_csv('./naked trades/Shares.csv')
-df = df.loc['2017-01-01':]
-# print(df)
-df['Epic']=df['Epic'].str.upper()
-df['Epic']=df['Epic'].replace(dict_from_csv)
-df = df.dropna(subset='Epic')
-df = df[df.Epic != 'Delisted']
-# print(dict_from_csv)
-print(df)
 
 unique = df['Epic'].unique()
 print(unique)
--- market_data.py
+++ market_data.py
@@ -7,23 +7,38 @@ import yfinance as yf
 from yahoo_fin import stock_info as si
 import shutil
 import requests
+from datetime import datetime
 
 ## Naked Trades ##
-def download_naked_trades():
+def download_naked_trades(earliest_date='2017-01-01'):
     # https://stackoverflow.com/questions/10556048/how-to-extract-tables-from-websites-in-python
     url = 'https://www.nakedtrader.co.uk/agree.htm?agree=1'
     html = requests.get(url).content
     df_list = pd.read_html(html)
 
-    df_Shares=df_list[0]
-    df_Spread_Longs=df_list[1]
-    df_Spread_Shorts=df_list[2]
-
-    df_Shares.rename(columns={'Buy Date':'Date'}, inplace=True)
+    # clean up data
+    dict_from_csv = pd.read_csv('./naked trades/MAP.csv', header=0, index_col=0).squeeze('columns').to_dict()
+    for i in range(2):
+        df_list[i].rename(columns = {'Buy Date':'Date'}, inplace = True) #rename for indexing
+        df_list[i]['Date'] = pd.to_datetime(df_list[i]['Date'], format = '%d/%m/%Y', errors = 'coerce') #ensure UK date format , '%d/%m/%Y'
+        df_list[i] = df_list[i][(df_list[i]['Date'] > earliest_date)] #cutoff date
+        df_list[i]['Sell Date'] = pd.to_datetime(df_list[i]['Sell Date'], format = '%d/%m/%Y', errors = 'coerce') #ensure UK date format , '%d/%m/%Y'
+        
+        df_list[i]['Epic'] = df_list[i]['Epic'].str.upper() #uppercase before replacing
+        df_list[i]['Epic'] = df_list[i]['Epic'].replace(dict_from_csv) #replace bad/delisted tickers
+        df_list[i] = df_list[i].dropna(subset = 'Epic') #drop null tickers
+        df_list[i] = df_list[i].dropna(subset = 'Date') #drop null dates
+        df_list[i] = df_list[i][df_list[i].Epic != 'Delisted'] #drop delisted tickers
+    
+    df_Shares = df_list[0]
+    df_Spread_Longs = df_list[1]
+    df_Spread_Shorts = df_list[2]
 
     df_Shares.to_csv('./naked trades/Shares.csv', index=False)
     df_Spread_Longs.to_csv('./naked trades/Spread_Longs.csv', index=False)
     df_Spread_Shorts.to_csv('./naked trades/Spread_Shorts.csv', index=False)
+
+    print(f'naked trades downloaded from {earliest_date}')
 
 ## Handle Files ##
 def delete_files_in_dir(directory_name):  # Delete all files in a directory
@@ -81,7 +96,7 @@ def get_stock_data(ticker, interval=config.INTERVAL, period=config.PERIOD):
 def df_from_csv(file_path):
     # pull some data - when writing and reading back from csv the index is converted to an object type so need to convert back to datetime64 if to be used with finplot
     df = pd.read_csv(file_path)
-    df.Date = pd.to_datetime(df.Date, format='%d/%m/%Y') #ensure UK date format
+    # df.Date = pd.to_datetime(df.Date, format='%d/%m/%Y') #ensure UK date format
     
     # format it in pandas
     df = df.astype({'Date':'datetime64[ns]'})
